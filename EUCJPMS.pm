1d2096e89a61d721c6557638f675e9c4201f70c1
NARUSE, Yui <naruse@airemix.com>
2006-06-23 19:50:08 +0000

    Import of NARUSE/Encode-EUCJPMS-0.06 from CPAN.
    gitpan-cpan-distribution: Encode-EUCJPMS
    gitpan-cpan-version:      0.06
    gitpan-cpan-path:         NARUSE/Encode-EUCJPMS-0.06.tar.gz
    gitpan-cpan-author:       NARUSE
    gitpan-cpan-maturity:     released

--- EUCJPMS.pm
+++ EUCJPMS.pm
@@ -1,23 +1,172 @@
 #
-# $Id: EUCJPMS.pm 6 2005-10-12 19:43:20Z naruse $
+# $Id: EUCJPMS.pm 5 2006-06-23 17:26:15Z naruse $
 #
 package Encode::EUCJPMS;
-our $VERSION = "0.05";
+use strict;
+our $VERSION = "0.06";
  
-use Encode;
+use Encode qw(:fallbacks);
 use XSLoader;
 XSLoader::load(__PACKAGE__,$VERSION);
 
 Encode::define_alias(qr/\beuc-?jp-?ms$/i =>  '"eucJP-ms"');
 Encode::define_alias(qr/\beuc-?jp-?win$/i => '"eucJP-ms"');
-Encode::define_alias(qr/\bcp51932$/i     =>  '"cp51932"');
+
+for my $name ('cp50220','cp50221'){ #, 'cp50222'
+    my $h2z     = ($name eq 'cp50220')    ? 1 : 0;
+    my $jis0212 = 0;
+
+    $Encode::Encoding{$name} =
+        bless {
+               Name      =>   $name,
+               h2z       =>   $h2z,
+               jis0212   =>   $jis0212,
+              } => __PACKAGE__;
+}
+
+use base qw(Encode::Encoding);
+
+# we override this to 1 so PerlIO works
+sub needs_lines { 1 }
+
+use Encode::CJKConstants qw(:all);
+
+#
+# decode is identical for all 2022 variants
+#
+
+sub decode($$;$)
+{
+    my ($obj, $str, $chk) = @_;
+    my $residue = '';
+    if ($chk){
+	$str =~ s/([^\x00-\x7f].*)$//so and $residue = $1;
+    }
+    $residue .= jis_euc(\$str);
+    $_[1] = $residue if $chk;
+    return Encode::decode('cp51932', $str, FB_PERLQQ);
+}
+
+#
+# encode is different
+#
+
+sub encode($$;$)
+{
+    require Encode::JP::H2Z;
+    my ($obj, $utf8, $chk) = @_;
+    # empty the input string in the stack so perlio is ok
+    $_[1] = '' if $chk;
+    my ($h2z, $jis0212) = @$obj{qw(h2z jis0212)};
+    my $octet = Encode::encode('cp51932', $utf8, FB_PERLQQ) ;
+    $h2z and &Encode::JP::H2Z::h2z(\$octet);
+    euc_jis(\$octet, $jis0212);
+    return $octet;
+}
+
+#
+# cat_decode
+#
+my $re_scan_jis_g = qr{
+   \G ( ($RE{JIS_0212}) |  $RE{JIS_0208}  |
+        ($RE{ISO_ASC})  | ($RE{JIS_KANA}) | )
+      ([^\e]*)
+}x;
+sub cat_decode { # ($obj, $dst, $src, $pos, $trm, $chk)
+    my ($obj, undef, undef, $pos, $trm) = @_; # currently ignores $chk
+    my ($rdst, $rsrc, $rpos) = \@_[1,2,3];
+    local ${^ENCODING};
+    use bytes;
+    my $opos = pos($$rsrc);
+    pos($$rsrc) = $pos;
+    while ($$rsrc =~ /$re_scan_jis_g/gc) {
+	my ($esc, $esc_0212, $esc_asc, $esc_kana, $chunk) =
+	  ($1, $2, $3, $4, $5);
+
+	unless ($chunk) { $esc or last;  next; }
+
+	if ($esc && !$esc_asc) {
+	    $chunk =~ tr/\x21-\x7e/\xa1-\xfe/;
+	    if ($esc_kana) {
+		$chunk =~ s/([\xa1-\xdf])/\x8e$1/og;
+	    } elsif ($esc_0212) {
+		$chunk =~ s/([\xa1-\xfe][\xa1-\xfe])/\x8f$1/og;
+	    }
+	    $chunk = Encode::decode('cp51932', $chunk, 0);
+	}
+	elsif ((my $npos = index($chunk, $trm)) >= 0) {
+	    $$rdst .= substr($chunk, 0, $npos + length($trm));
+	    $$rpos += length($esc) + $npos + length($trm);
+	    pos($$rsrc) = $opos;
+	    return 1;
+	}
+	$$rdst .= $chunk;
+	$$rpos = pos($$rsrc);
+    }
+    $$rpos = pos($$rsrc);
+    pos($$rsrc) = $opos;
+    return '';
+}
+
+# JIS<->EUC
+my $re_scan_jis = qr{
+   (?:($RE{JIS_0212})|$RE{JIS_0208}|($RE{ISO_ASC})|($RE{JIS_KANA}))([^\e]*)
+}x;
+
+sub jis_euc {
+    local ${^ENCODING};
+    my $r_str = shift;
+    $$r_str =~ s($re_scan_jis)
+    {
+	my ($esc_0212, $esc_asc, $esc_kana, $chunk) =
+	   ($1, $2, $3, $4);
+	if (!$esc_asc) {
+	    $chunk =~ tr/\x21-\x7e/\xa1-\xfe/;
+	    if ($esc_kana) {
+		$chunk =~ s/([\xa1-\xdf])/\x8e$1/og;
+	    }
+	    elsif ($esc_0212) {
+		$chunk =~ s/([\xa1-\xfe][\xa1-\xfe])/\x8f$1/og;
+	    }
+	}
+	$chunk;
+    }geox;
+    my ($residue) = ($$r_str =~ s/(\e.*)$//so);
+    return $residue;
+}
+
+sub euc_jis{
+    no warnings qw(uninitialized);
+    my $r_str = shift;
+    my $jis0212 = shift;
+    $$r_str =~ s{
+	((?:$RE{EUC_C})+|(?:$RE{EUC_KANA})+|(?:$RE{EUC_0212})+)
+	}{
+	    my $chunk = $1;
+	    my $esc =
+		( $chunk =~ tr/\x8E//d ) ? $ESC{KANA} :
+		    ( $chunk =~ tr/\x8F//d ) ? $ESC{JIS_0212} :
+			$ESC{JIS_0208};
+	    if ($esc eq $ESC{JIS_0212} && !$jis0212){
+		# fallback to '?'
+		$chunk =~ tr/\xA1-\xFE/\x3F/;
+	    }else{
+		$chunk =~ tr/\xA1-\xFE/\x21-\x7E/;
+	    }
+	    $esc . $chunk . $ESC{ASC};
+	}geox;
+    $$r_str =~
+	s/\Q$ESC{ASC}\E
+	    (\Q$ESC{KANA}\E|\Q$ESC{JIS_0212}\E|\Q$ESC{JIS_0208}\E)/$1/gox;
+    $$r_str;
+}
 
 1;
 __END__
 
 =head1 NAME
 
-Encode::EUCJPMS - eucJP-ms
+Encode::EUCJPMS - Microsoft Compatible Encodings for Japanese
 
 =head1 SYNOPSIS
 
@@ -28,14 +177,16 @@ Encode::EUCJPMS - eucJP-ms
 
 =head1 ABSTRACT
 
-This module implements encodings that covers eucJP-ms.
+This module implements Microsoft compatible encodings for Japanese.
 Encodings supported are as follows.
 
   Canonical     Alias                                      Description
   --------------------------------------------------------------------
   eucJP-ms      qr/\beuc-?jp-?ms$/i                           eucJP-ms
                 qr/\beuc-?jp-?win$/i
-  cp51932       qr/\bcp51932$/i                   Windows Codepage 932
+  cp51932                                       Windows Codepage 51932
+  cp50220                                       Windows Codepage 50220
+  cp50221                                       Windows Codepage 50221
   --------------------------------------------------------------------
 
 =head1 DESCRIPTION
@@ -69,7 +220,7 @@ NARUSE, Yui E<lt>naruse@airemix.comE<gt>
 
 =head1 COPYRIGHT
 
-Copyright (C) 2005 NARUSE, Yui E<lt>naruse@airemix.comE<gt>
+Copyright (C) 2005-2006 NARUSE, Yui E<lt>naruse@airemix.comE<gt>
 
 This program is free software; you can redistribute it and/or 
 modify it under the same terms as Perl itself.
